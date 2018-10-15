60ad106c9362f5fb81ef890f49084436e385630f
Ehbraheem <ehbraheem@gmail.com>
2018-10-15 19:56:02 +0100

    Migrated User Table
    * Create User table in Database
    * Edited migration file to match specification

--- db/migrate/20181015172126_devise_token_auth_create_users.rb
+++ db/migrate/20181015172126_devise_token_auth_create_users.rb
@@ -1,10 +1,10 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     
     create_table(:users) do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
+      t.string :username, :null => false, :default => ""
 
       ## Database authenticatable
       t.string :encrypted_password, :null => false, :default => ""
@@ -48,9 +48,18 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, [:uid, :provider],     unique: true
+    add_index :users, [:username, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
+  end
+
+  def down
+    remove_index :users, :email
+    remove_index :users, :reset_password_token
+    remove_index :users, :confirmation_token
+    remove_index :users, [:username, :provider]
+
+    drop_table :users
   end
 end
