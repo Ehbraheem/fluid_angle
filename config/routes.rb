8343ddf6e65f2cc0e8fa2a77c869b3b8451d2547
Ehbraheem <ehbraheem@gmail.com>
2018-10-15 20:00:10 +0100

    Moved Users Session route below API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'user'
   scope :api, defaults: {format: :json} do
     resources :foos
+    mount_devise_token_auth_for 'User', at: 'user'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
