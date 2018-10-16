48d24c10725d57546dfa64be51f9afb82fb7efa2
Ehbraheem <ehbraheem@gmail.com>
2018-10-16 12:43:32 +0100

    Fixed typo causing bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :contacts, inverse_of: :user, dependent: :destroy, 
+  has_many :contacts, inverse_of: :user, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
