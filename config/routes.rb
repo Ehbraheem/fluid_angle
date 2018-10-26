a76a0c7d1599e37c409a5a270a8c41e7bf65d7b4
Ehbraheem <ehbraheem@gmail.com>
2018-10-26 18:38:46 +0100

    Added spec for Contact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'auth/whoami'
   get 'auth/checkme'
-  resources :contacts
   scope :api, defaults: {format: :json} do
+    resources :contacts
     resources :foos
     mount_devise_token_auth_for 'User', at: 'user'
   end
--- spec/routing/foos_routing_spec.rb
+++ spec/routing/foos_routing_spec.rb
@@ -1,29 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe FoosController, type: :routing do
-  describe 'routing' do
-    it 'routes to #index' do
-      expect(get: foos_path).to route_to('foos#index', format: :json)
-    end
-
-    it 'routes to #show' do
-      expect(get: foo_path(1)).to route_to('foos#show', id: '1', format: :json)
-    end
-
-    it 'routes to #create' do
-      expect(post: foos_path).to route_to('foos#create', format: :json)
-    end
-
-    it 'routes to #update via PUT' do
-      expect(put: foo_path(1)).to route_to('foos#update', id: '1', format: :json)
-    end
-
-    it 'routes to #update via PATCH' do
-      expect(patch: foo_path(1)).to route_to('foos#update', id: '1', format: :json)
-    end
-
-    it 'routes to #destroy' do
-      expect(delete: foo_path(1)).to route_to('foos#destroy', id: '1', format: :json)
-    end
-  end
+  it_should_behave_like 'routing', :foo
 end
